5ada28923f12566312430dbb7a008191ad5328da
Andres Galindo <agalin920@gmail.com>
2018-09-03 14:41:19 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import q12 from './assets/12.png';
+import q13 from './assets/13.png';
+import q14 from './assets/14.png';
+import q15 from './assets/15.png';
+import q16 from './assets/16.png';
+import q17 from './assets/17.png';
+import q18 from './assets/18.png';
+import q19 from './assets/19.png';
+import q20 from './assets/20.png';
+import q21 from './assets/21.png';
 
 //Automata description
 const automata = {
@@ -84,10 +93,12 @@ class App extends Component {
     this.state = {
       inputText:"",
       isAccepted:false,
+      imgSrc:q12
     };
 
     this.handleChange=this.handleChange.bind(this);
     this.getNextState=this.getNextState.bind(this);
+    this.getImage=this.getImage.bind(this);
   }
 
   handleChange(event){
@@ -97,7 +108,6 @@ class App extends Component {
     this.setState({inputText:event.target.value});
     for(let i=0;i<event.target.value.length;i++){
       currentState = this.getNextState(currentState,event.target.value.charAt(i));
-      console.log(currentState);
     }
 
 
@@ -149,8 +159,35 @@ class App extends Component {
  
 }
 
+getImage(){
+    if((this.state.inputText.includes("e") || this.state.inputText.includes("E")) && !isNaN(this.state.inputText.charAt(this.state.inputText.length-1))&& !isNaN(this.state.inputText.charAt(0))){
+            return q19;
+        }
+    else if((this.state.inputText.includes("e") || this.state.inputText.includes("E")) && (this.state.inputText.includes("+")||this.state.inputText.includes("-")) && !isNaN(this.state.inputText.charAt(0))){
+        return q17;
+    }
+    else if((this.state.inputText.includes("e") || this.state.inputText.includes("E"))&& !isNaN(this.state.inputText.charAt(0))){
+        return q16;
+    }
+    else if(this.state.inputText.includes(".") && !isNaN(this.state.inputText.charAt(0)) && !isNaN(this.state.inputText.charAt(this.state.inputText.length-1)) ){
+        console.log("gsd",isNaN(this.state.inputText.charAt(this.state.inputText.length-1)))
+        return q21;
+    }
+    else if(this.state.inputText.includes(".") && !isNaN(this.state.inputText.charAt(0))){
+        return q14;
+    }
+    else if(!isNaN(this.state.inputText.charAt(0)) && this.state.inputText !== ""){
+        return q20;
+    }
+    
+    else{
+        return q12;
+    }
+    
+}
 
   render() {
+    let currImage = this.getImage();
     return (
       <div className="App">
         <header className="App-header">
@@ -160,6 +197,8 @@ class App extends Component {
         <body>
           <h1 className="input-stream" style={{color: this.state.isAccepted === true ? 'green' : 'red'}}>{this.state.inputText}</h1>
           <input type="text" placeholder="Introduzca una cadena" onChange={this.handleChange}/>
+          <br/>
+          <img src={currImage}/>
         </body>
       </div>
     );
